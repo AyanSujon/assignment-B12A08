0f5606f120a094517c37f5aee5349060475fd7cd
Ayan <sujon.seo.bd@gmail.com>
2025-10-07 22:14:03 +0600

    added all resources

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Components/Footer';
+
+const MainLayout = () => {
+    return (
+        <>
+        <div className='flex flex-col min-h-screen border-1 border-red-500'>
+        <Navbar/>
+        <div className='flex-1 '>
+            <Outlet/>
+        </div>
+        <Footer/>
+        </div>
+        
+        
+        
+        </>
+    );
+};
+
+export default MainLayout;
